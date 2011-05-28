2a813e0553a84f695b4fbf7ca2c2621571035b44
Narendran Thangarajan <narendran.thangarajan@gmail.com>
2011-05-28 20:32:52 +0530

    First Commit

--- cuiclient.py
+++ cuiclient.py
@@ -1,7 +1,7 @@
 import sys,twitter,subprocess,time
 #subprocess is to execute linux shell commands in python
 
-client = twitter.Api(consumer_key='yMNI0YTEKdNBjX5GyNZBw',consumer_secret='OghfzFIesnuCjqKo7SCL2lqH3iH5wQDe3yU9NoIeio', access_token_key='306646649-t3xZEcLvihWUvdWF38FE1wFMsYjB2psG5nYwMaB5', access_token_secret='c1vBemCblKpwXE99coKZHrl1QSLLtUZS7X7AYP4n0g')
+client = twitter.Api(consumer_key='',consumer_secret='', access_token_key='', access_token_secret='')
 while True :
     for arg in sys.argv:
          if(arg!='cuiclient.py'):
